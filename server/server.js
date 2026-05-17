3b91af13f66f44e20691322275dedc93a9cce63a
afnanahmed2 <afnana7med10@gmail.com>
2026-05-17 10:43:43 +0400

    Updated version

--- server/server.js
+++ server/server.js
@@ -1688,6 +1688,7 @@ app.use((req, res) => {
 ========================================================= */
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`\n========================================`);
